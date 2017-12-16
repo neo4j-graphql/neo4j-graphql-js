092779f1b8b869d814324926837cadbc1d74419f
johnymontana <lyonwj@gmail.com>
2017-12-15 17:44:50 -0700

    add utils module

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+export function parseArgs(args) {
+  if (!args) {
+    return {};
+  }
+
+  if (args.length === 0) {
+    return {};
+  }
+
+  return args.reduce( (acc, arg) => {
+
+    switch (arg.value.kind) {
+      case "IntValue":
+        acc[arg.name.value] = parseInt(arg.value.value);
+        break;
+      case "FloatValue":
+        acc[arg.name.value] = parseFloat(arg.value.value);
+        break;
+      default:
+        acc[arg.name.value] = arg.value.value;
+    }
+
+    return acc;
+  }, {})
+}
+
+function getDefaultArguments(fieldName, schemaType) {
+  // FIXME: check that these things exist
+
+  try {
+    return schemaType._fields[fieldName].args.reduce((acc, arg) => {
+      acc[arg.name] = arg.defaultValue;
+      return acc;
+    }, {})
+  } catch (err) {
+    return {};
+  }
+}
+
+export function cypherDirectiveArgs(variable, headSelection, schemaType) {
+  const defaultArgs = getDefaultArguments(headSelection.name.value, schemaType)//{"this": variable};
+  const schemaArgs = {}; // FIXME: what's the differenc between schemargs and defaultargs?
+  const queryArgs = parseArgs(headSelection.arguments);
+  console.log(queryArgs);
+
+  let args = JSON.stringify(Object.assign(defaultArgs, queryArgs)).replace(/\"([^(\")"]+)\":/g,"$1:");
+
+  return `{this: ${variable},` + args.substring(1);
+
+}
