80971e3076a149d6dc3c4dd721b19b894780f5aa
Charlie Briggs <charlie.briggs@ft.com>
2018-06-22 10:47:43 +0100

    Env var rename for consistency

--- scripts/install-neo4j.sh
+++ scripts/install-neo4j.sh
@@ -2,8 +2,8 @@
 
 if [ ! -d "neo4j/data/databases/graph.db" ]; then
     mkdir -p neo4j
-    wget dist.neo4j.org/neo4j-$NEO4j_DIST-$NEO4J_VERSION-unix.tar.gz
-    tar -xzf neo4j-$NEO4j_DIST-$NEO4J_VERSION-unix.tar.gz -C neo4j --strip-components 1
+    wget dist.neo4j.org/neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz
+    tar -xzf neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz -C neo4j --strip-components 1
     neo4j/bin/neo4j-admin set-initial-password letmein
     curl -L https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/$APOCH_VERSION/apoc-$APOCH_VERSION-all.jar > ./neo4j/plugins/apoc-$APOCH_VERSION-all.jar
     wget https://s3.amazonaws.com/neo4j-sandbox-usecase-datastores/v3_3/recommendations.db.zip
