be81f16bca7b9626348ae4b7b6ca0918f7dc4fc6
Charlie Briggs <charlie.briggs@ft.com>
2018-06-22 10:35:54 +0100

    Split install and startup
    This allows the neo4j install to be cached for separate node versions

--- scripts/install-neo4j.sh
+++ scripts/install-neo4j.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -d "neo4j/data/databases/graph.db" ]; then
+    mkdir -p neo4j
     wget dist.neo4j.org/neo4j-$NEO4j_DIST-$NEO4J_VERSION-unix.tar.gz
     tar -xzf neo4j-$NEO4j_DIST-$NEO4J_VERSION-unix.tar.gz -C neo4j --strip-components 1
     neo4j/bin/neo4j-admin set-initial-password letmein
