2db127296eef2c9bd6c869907cdd47f481c24d7c
johnymontana <lyonwj@gmail.com>
2018-12-05 13:57:45 -0700

    always set upgrade store flag

--- scripts/start-neo4j.sh
+++ scripts/start-neo4j.sh
@@ -6,8 +6,8 @@ if [ ! -d "neo4j/data/databases/graph.db" ]; then
     echo "Neo4j not installed correctly, run ./scripts/install_neo4j"
     exit 1
 else
-    [ "$NEO4J_VERSION" == "3.4.0" ] && FLAGS="dbms.allow_upgrade=true" || FLAGS=""
-    echo "$FLAGS" >> ./neo4j/conf/neo4j.conf
+    #[ "$NEO4J_VERSION" == "3.4.0" ] && FLAGS="dbms.allow_upgrade=true" || FLAGS=""
+    echo "dbms.allow_upgrade=true" >> ./neo4j/conf/neo4j.conf
     # Set initial and max heap to workaround JVM in docker issues
     dbms_memory_heap_initial_size="2048m" dbms_memory_heap_max_size="2048m" ./neo4j/bin/neo4j start
     echo "Waiting up to 2 minutes for neo4j bolt port ($BOLT_PORT)"
