a1b977a2ab66afda1aab02bad5a756cb8e9e1ef9
johnymontana <lyonwj@gmail.com>
2018-12-06 14:11:46 -0700

    add temporal type query arg tests

--- test/integration.js
+++ test/integration.js
@@ -915,3 +915,77 @@ test.serial('Temporal - create node with only a temporal property', async t => {
       t.fail(error);
     });
 });
+
+test.serial('Temporal - temporal query argument, components', async t => {
+  t.plan(1);
+
+  let expected = {
+    data: {
+      OnlyDate: [
+        {
+          __typename: 'OnlyDate',
+          date: {
+            __typename: '_Neo4jDate',
+            formatted: '2020-11-10'
+          }
+        }
+      ]
+    }
+  };
+
+  await client
+    .query({
+      query: gql`
+        {
+          OnlyDate(date: { day: 10, month: 11, year: 2020 }) {
+            date {
+              formatted
+            }
+          }
+        }
+      `
+    })
+    .then(data => {
+      t.deepEqual(data.data, expected.data);
+    })
+    .catch(error => {
+      t.fail(error);
+    });
+});
+
+test.serial('Temporal - temporal query argument, formatted', async t => {
+  t.plan(1);
+
+  let expected = {
+    data: {
+      OnlyDate: [
+        {
+          __typename: 'OnlyDate',
+          date: {
+            __typename: '_Neo4jDate',
+            formatted: '2020-11-10'
+          }
+        }
+      ]
+    }
+  };
+
+  await client
+    .query({
+      query: gql`
+        {
+          OnlyDate(date: { formatted: "2020-11-10" }) {
+            date {
+              formatted
+            }
+          }
+        }
+      `
+    })
+    .then(data => {
+      t.deepEqual(data.data, expected.data);
+    })
+    .catch(error => {
+      t.fail(error);
+    });
+});
