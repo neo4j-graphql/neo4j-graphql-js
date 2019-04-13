3ef0efee6e82269d48a7e38a6e18ee4dbe01b828
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2019-04-12 17:00:04 -0700

    updates for generated filter argument

--- test/configTest.js
+++ test/configTest.js
@@ -147,7 +147,7 @@ test.cb(
 
     const queryType = `
 type Query {
-  Tweet(id: ID, timestamp: _Neo4jDateTimeInput, text: String, _id: String, first: Int, offset: Int, orderBy: [_TweetOrdering]): [Tweet]
+  Tweet(id: ID, timestamp: _Neo4jDateTimeInput, text: String, _id: String, first: Int, offset: Int, orderBy: [_TweetOrdering], filter: _TweetFilter): [Tweet]
 }
 `;
 
@@ -169,7 +169,7 @@ test.cb('Config - augmentSchema - specify types to exclude for query', t => {
 
   const queryType = `
 type Query {
-  Tweet(id: ID, timestamp: _Neo4jDateTimeInput, text: String, _id: String, first: Int, offset: Int, orderBy: [_TweetOrdering]): [Tweet]
+  Tweet(id: ID, timestamp: _Neo4jDateTimeInput, text: String, _id: String, first: Int, offset: Int, orderBy: [_TweetOrdering], filter: _TweetFilter): [Tweet]
 }
 `;
 
