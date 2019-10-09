8094500080a611e200840f927ca2df7b24c3bff6
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2019-10-08 20:18:19 -0700

    avoid overwriting existent filtering test file

--- test/helpers/tck/parseTck.js
+++ test/helpers/tck/parseTck.js
@@ -1,6 +1,6 @@
 import { generateTestFile } from './parser';
 
 const TCK_FILE = './test/helpers/tck/filterTck.md';
-const TEST_FILE = './test/tck/filterTest.test.js';
+const TEST_FILE = './test/unit/filterTests.test.js';
 
 generateTestFile(TCK_FILE, TEST_FILE);
