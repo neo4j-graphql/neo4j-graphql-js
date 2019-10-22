e803f09c32d8597d81d451e5e059e98381846649
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2019-10-22 13:36:10 -0700

    schema type support, documentation comments

--- src/augment/types/types.js
+++ src/augment/types/types.js
@@ -12,7 +12,6 @@ import {
   DirectiveDefinition,
   getDirective
 } from '../directives';
-import { shouldAugmentType } from '../augment';
 import {
   buildName,
   buildNamedType,
@@ -25,21 +24,32 @@ import {
   unwrapNamedType,
   getFieldDefinition
 } from '../fields';
+import { augmentNodeType } from './node/node';
 import { RelationshipDirectionField } from '../types/relationship/relationship';
 
+// The prefix added to the name of any type representing a managed Neo4j data type
 export const Neo4jTypeName = `_Neo4j`;
 
+/**
+ * An enum describing Neo4j entity types, used in type predicate functions
+ */
 export const Neo4jStructuralType = {
   NODE: 'Node',
   RELATIONSHIP: 'Relationship'
 };
 
+/**
+ * An enum describing the semantics of default GraphQL operation types
+ */
 export const OperationType = {
   QUERY: 'Query',
   MUTATION: 'Mutation',
   SUBSCRIPTION: 'Subscription'
 };
 
+/**
+ * A map of the semantics of the GraphQL type system to Neo4j data types
+ */
 // https://neo4j.com/docs/cypher-manual/current/syntax/values/#cypher-values
 export const Neo4jDataType = {
   PROPERTY: {
@@ -60,43 +70,102 @@ export const Neo4jDataType = {
   }
 };
 
+/**
+ * A predicate function for identifying type definitions representing
+ * a Neo4j node entity
+ */
 export const isNodeType = ({ definition }) =>
   interpretType({ definition }) === Neo4jStructuralType.NODE;
 
+/**
+ * A predicate function for identifying type definitions representing
+ * a Neo4j relationship entity
+ */
 export const isRelationshipType = ({ definition }) =>
   interpretType({ definition }) === Neo4jStructuralType.RELATIONSHIP;
 
+/**
+ * A predicate function for identifying a GraphQL Object Type definition
+ */
 export const isObjectTypeDefinition = ({ definition = {} }) =>
   definition.kind === Kind.OBJECT_TYPE_DEFINITION;
 
+/**
+ * A predicate function for identifying a GraphQL Input Object Type definition
+ */
 export const isInputObjectTypeDefinition = ({ definition = {} }) =>
   definition.kind === Kind.INPUT_OBJECT_TYPE_DEFINITION;
 
+/**
+ * A predicate function for identifying a GraphQL Interface Type definition
+ */
 export const isInterfaceTypeDefinition = ({ definition = {} }) =>
   definition.kind === Kind.INTERFACE_TYPE_DEFINITION;
 
+/**
+ * A predicate function for identifying a GraphQL Union definition
+ */
 export const isUnionTypeDefinition = ({ definition = {} }) =>
   definition.kind === Kind.UNION_TYPE_DEFINITION;
 
-export const isOperationTypeDefinition = ({ definition = {} }) =>
-  isQueryTypeDefinition({ definition }) ||
-  isMutationTypeDefinition({ definition }) ||
-  isSubscriptionTypeDefinition({ definition });
+/**
+ * A predicate function for identifying a GraphQL operation type definition
+ */
+export const isOperationTypeDefinition = ({
+  definition = {},
+  operationTypeMap
+}) =>
+  isQueryTypeDefinition({ definition, operationTypeMap }) ||
+  isMutationTypeDefinition({ definition, operationTypeMap }) ||
+  isSubscriptionTypeDefinition({ definition, operationTypeMap });
 
-export const isQueryTypeDefinition = ({ definition }) =>
-  definition.name && definition.name.value === OperationType.QUERY;
+/**
+ * A predicate function for identifying the GraphQL Query type definition
+ */
+export const isQueryTypeDefinition = ({ definition, operationTypeMap }) =>
+  definition.name && operationTypeMap[OperationType.QUERY]
+    ? definition.name.value === operationTypeMap[OperationType.QUERY].name.value
+    : false;
 
-export const isMutationTypeDefinition = ({ definition }) =>
-  definition.name && definition.name.value === OperationType.MUTATION;
+/**
+ * A predicate function for identifying the GraphQL Mutation type definition
+ */
+export const isMutationTypeDefinition = ({ definition, operationTypeMap }) =>
+  definition.name && operationTypeMap[OperationType.MUTATION]
+    ? definition.name.value ===
+      operationTypeMap[OperationType.MUTATION].name.value
+    : false;
 
-export const isSubscriptionTypeDefinition = ({ definition }) =>
-  definition.name && definition.name.value === OperationType.SUBSCRIPTION;
+/**
+ * A predicate function for identifying the GraphQL Subscription type definition
+ */
+export const isSubscriptionTypeDefinition = ({
+  definition,
+  operationTypeMap
+}) =>
+  definition.name && operationTypeMap[OperationType.SUBSCRIPTION]
+    ? definition.name.value ===
+      operationTypeMap[OperationType.SUBSCRIPTION].name.value
+    : false;
 
+/**
+ * A predicate function for identifying a GraphQL type definition representing
+ * complex Neo4j property types (Temporal, Spatial) managed by the translation process
+ */
+export const isNeo4jPropertyType = ({ type }) => isNeo4jTemporalType({ type });
+
+/**
+ * A predicate function for identifying a GraphQL type definition representing
+ * a Neo4j Temporal type (Time, Date, DateTime, LocalTime, LocalDateTime)
+ * with a name that has already been transformed ('_Neo4j' prefix added)
+ */
 export const isNeo4jTemporalType = ({ type }) =>
   Object.values(TemporalType).some(name => type === `${Neo4jTypeName}${name}`);
 
-export const isNeo4jPropertyType = ({ type }) => isNeo4jTemporalType({ type });
-
+/**
+ * A predicate function for identifying which Neo4j entity type, if any, a given
+ * GraphQL type definition represents
+ */
 export const interpretType = ({ definition = {} }) => {
   const kind = definition.kind;
   // Get the structural types allows for this definition kind
@@ -132,7 +201,48 @@ export const interpretType = ({ definition = {} }) => {
   return neo4jType;
 };
 
-export const buildNeo4jTypes = ({ generatedTypeMap, config }) => {
+/**
+ * The main export for the augmentation process over prepared maps of
+ * GraphQL type definitions
+ */
+export const augmentTypes = ({
+  typeDefinitionMap,
+  typeExtensionDefinitionMap,
+  generatedTypeMap,
+  operationTypeMap = {},
+  config = {}
+}) => {
+  Object.entries({
+    ...typeDefinitionMap,
+    ...operationTypeMap
+  }).forEach(([typeName, definition]) => {
+    if (isNodeType({ definition })) {
+      [definition, generatedTypeMap, operationTypeMap] = augmentNodeType({
+        typeName,
+        definition,
+        typeDefinitionMap,
+        generatedTypeMap,
+        operationTypeMap,
+        config
+      });
+      generatedTypeMap[typeName] = definition;
+    } else {
+      generatedTypeMap[typeName] = definition;
+    }
+    return definition;
+  });
+  generatedTypeMap = buildNeo4jTypes({
+    generatedTypeMap,
+    config
+  });
+  return [typeExtensionDefinitionMap, generatedTypeMap, operationTypeMap];
+};
+
+/**
+ * Builds the GraphQL AST type definitions that represent complex Neo4j
+ * property types (Temporal, Spatial) managed by the translation process
+ */
+const buildNeo4jTypes = ({ generatedTypeMap, config }) => {
   generatedTypeMap = buildTemporalTypes({
     typeMap: generatedTypeMap,
     config
@@ -140,6 +250,11 @@ export const buildNeo4jTypes = ({ generatedTypeMap, config }) => {
   return generatedTypeMap;
 };
 
+/**
+ * Builds a GraphQL Object type and Input Object type representing
+ * the input and output schema for a complex Neo4j property type,
+ * ex: _Neo4jTime, _Neo4jTimeInput, etc.
+ */
 export const buildNeo4jType = ({
   inputTypeName,
   inputFields,
@@ -158,6 +273,11 @@ export const buildNeo4jType = ({
   return typeMap;
 };
 
+/**
+ * Applies the Neo4jTypeName prefix to any Field or Input Value definition
+ * with a type representing a complex Neo4j property type, to align with the
+ * type names expected by the translation process
+ */
 export const transformNeo4jTypes = ({ definitions = [], config }) => {
   const inputTypeSuffix = `Input`;
   return visit(definitions, {
@@ -194,73 +314,6 @@ export const transformNeo4jTypes = ({ definitions = [], config }) => {
         field.type = buildNamedType(unwrappedType);
       }
       return field;
-    }
-  });
-};
-
-export const initializeOperationTypes = ({
-  typeDefinitionMap,
-  config = {}
-}) => {
-  const types = Object.keys(typeDefinitionMap);
-  const queryTypeName = OperationType.QUERY;
-  const queryTypeNameLower = queryTypeName.toLowerCase();
-  const mutationTypeName = OperationType.MUTATION;
-  const mutationTypeNameLower = mutationTypeName.toLowerCase();
-  const subscriptionTypeName = OperationType.SUBSCRIPTION;
-  let queryType = typeDefinitionMap[queryTypeName];
-  let mutationType = typeDefinitionMap[mutationTypeName];
-  let subscriptionType = typeDefinitionMap[subscriptionTypeName];
-  if (
-    hasNonExcludedNodeType(
-      types,
-      typeDefinitionMap,
-      queryTypeNameLower,
-      config
-    ) &&
-    !queryType &&
-    config.query
-  ) {
-    queryType = buildObjectType({
-      name: buildName({ name: queryTypeName })
-    });
-  }
-  if (
-    hasNonExcludedNodeType(
-      types,
-      typeDefinitionMap,
-      mutationTypeNameLower,
-      config
-    ) &&
-    !mutationType &&
-    config.mutation
-  ) {
-    mutationType = buildObjectType({
-      name: buildName({ name: mutationTypeName })
-    });
-  }
-  const operationTypeMap = {};
-  if (queryType) {
-    operationTypeMap[OperationType.QUERY] = queryType;
-  }
-  if (mutationType) {
-    operationTypeMap[OperationType.MUTATION] = mutationType;
-  }
-  if (subscriptionType) {
-    operationTypeMap[OperationType.SUBSCRIPTION] = subscriptionType;
-  }
-  return [typeDefinitionMap, operationTypeMap];
-};
-
-const hasNonExcludedNodeType = (types, typeMap, rootType, config) => {
-  return types.find(e => {
-    const type = typeMap[e];
-    const typeName = type.name ? type.name.value : '';
-    if (typeName) {
-      return (
-        isNodeType({ definition: type }) &&
-        shouldAugmentType(config, rootType, typeName)
-      );
     }
   });
 };
