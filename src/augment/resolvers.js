1a707a595b3ef4316f1a793b48c95db868811028
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2019-10-22 13:30:38 -0700

    schema type support, documentation comments

--- src/augment/resolvers.js
+++ src/augment/resolvers.js
@@ -1,31 +1,57 @@
 import { neo4jgraphql } from '../index';
 import { OperationType } from '../augment/types/types';
 
-export const augmentResolvers = (augmentedTypeMap, resolvers, config) => {
-  let queryResolvers =
-    resolvers && resolvers[OperationType.QUERY]
-      ? resolvers[OperationType.QUERY]
-      : {};
-  const queryType = augmentedTypeMap[OperationType.QUERY];
+/**
+ * The main export for the generation of resolvers for the
+ * Query and Mutation API. Prevent overwriting.
+ */
+export const augmentResolvers = (
+  augmentedTypeMap,
+  operationTypeMap,
+  resolvers,
+  config
+) => {
+  // Persist and generate Query resolvers
+  let queryTypeName = OperationType.QUERY;
+  const queryType = operationTypeMap[queryTypeName];
   if (queryType) {
+    queryTypeName = queryType.name.value;
+    let queryResolvers =
+      resolvers && resolvers[queryTypeName] ? resolvers[queryTypeName] : {};
     queryResolvers = possiblyAddResolvers(queryType, queryResolvers, config);
     if (Object.keys(queryResolvers).length > 0) {
-      resolvers[OperationType.QUERY] = queryResolvers;
+      resolvers[queryTypeName] = queryResolvers;
     }
   }
-  let mutationResolvers =
-    resolvers && resolvers[OperationType.MUTATION]
-      ? resolvers[OperationType.MUTATION]
-      : {};
-  const mutationType = augmentedTypeMap[OperationType.MUTATION];
+  // Persist and generate Mutation resolvers
+  let mutationTypeName = OperationType.MUTATION;
+  const mutationType = operationTypeMap[mutationTypeName];
   if (mutationType) {
+    mutationTypeName = mutationType.name.value;
+    let mutationResolvers =
+      resolvers && resolvers[mutationTypeName]
+        ? resolvers[mutationTypeName]
+        : {};
     mutationResolvers = possiblyAddResolvers(
       mutationType,
       mutationResolvers,
       config
     );
     if (Object.keys(mutationResolvers).length > 0) {
-      resolvers[OperationType.MUTATION] = mutationResolvers;
+      resolvers[mutationTypeName] = mutationResolvers;
+    }
+  }
+  // Persist Subscription resolvers
+  let subscriptionTypeName = OperationType.SUBSCRIPTION;
+  const subscriptionType = operationTypeMap[subscriptionTypeName];
+  if (subscriptionType) {
+    subscriptionTypeName = subscriptionType.name.value;
+    let subscriptionResolvers =
+      resolvers && resolvers[subscriptionTypeName]
+        ? resolvers[subscriptionTypeName]
+        : {};
+    if (Object.keys(subscriptionResolvers).length > 0) {
+      resolvers[subscriptionTypeName] = subscriptionResolvers;
     }
   }
   // must implement __resolveInfo for every Interface type
@@ -44,6 +70,10 @@ export const augmentResolvers = (augmentedTypeMap, resolvers, config) => {
   return resolvers;
 };
 
+/**
+ * Generates resolvers for a given operation type, if
+ * any fields exist, for any resolver not provided
+ */
 const possiblyAddResolvers = (operationType, resolvers, config) => {
   let operationName = '';
   const fields = operationType ? operationType.fields : [];
@@ -54,6 +84,7 @@ const possiblyAddResolvers = (operationType, resolvers, config) => {
   return Object.keys(operationTypeMap).reduce((acc, t) => {
     // if no resolver provided for this operation type field
     operationName = operationTypeMap[t].name.value;
+    // If not provided
     if (acc[operationName] === undefined) {
       acc[operationName] = function(...args) {
         return neo4jgraphql(...args, config.debug);
@@ -63,6 +94,9 @@ const possiblyAddResolvers = (operationType, resolvers, config) => {
   }, resolvers);
 };
 
+/**
+ * Extracts resolvers from a schema
+ */
 export const extractResolversFromSchema = schema => {
   const _typeMap = schema && schema._typeMap ? schema._typeMap : {};
   const types = Object.keys(_typeMap);
@@ -91,6 +125,10 @@ export const extractResolversFromSchema = schema => {
   }, {});
 };
 
+/**
+ * Extracts field resolvers from a given type taken
+ * from a schema
+ */
 const extractFieldResolversFromSchemaType = type => {
   const fields = type._fields;
   const fieldKeys = fields ? Object.keys(fields) : [];
