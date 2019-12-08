7725668243cef1525c4854cd4758512c6752118b
Flaviu Matan Dragoste <flaviu.matan-dragoste@keysight.com>
2019-12-07 18:32:40 -0800

    Fix custom cypher fields of interface (array) type:  derived types params were missing

--- src/translate.js
+++ src/translate.js
@@ -107,7 +107,10 @@ export const customCypherField = ({
   // appropriately to the indexed keys produced in getFilterParams()
   const cypherFieldParamsIndex = paramIndex - 1;
   const fragmentTypeParams = fieldIsInterfaceType
-    ? derivedTypesParams(resolveInfo.schema, fieldType.ofType.astNode.name)
+    ? derivedTypesParams(
+        resolveInfo.schema,
+        fieldType.ofType.astNode.name.value
+      )
     : {};
 
   return {
@@ -122,7 +125,10 @@ export const customCypherField = ({
       cypherFieldParamsIndex
     )}}, true) | ${nestedVariable} {${
       fieldIsInterfaceType
-        ? `${fragmentType(nestedVariable, fieldType.ofType.astNode.name)},`
+        ? `${fragmentType(
+            nestedVariable,
+            fieldType.ofType.astNode.name.value
+          )},`
         : ''
     }${subSelection[0]}}]${fieldIsList ? '' : ')'}${skipLimit} ${commaIfTail}`,
     ...tailParams,
