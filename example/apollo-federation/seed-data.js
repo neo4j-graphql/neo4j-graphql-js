578aa89f3b342ed4b7cdfa0dd3cc823f4a2d81fe
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2020-05-20 20:02:07 -0700

    adds relationship property data

--- example/apollo-federation/seed-data.js
+++ example/apollo-federation/seed-data.js
@@ -4,6 +4,7 @@ export const seedData = {
       {
         id: '1',
         body: 'Love it!',
+        rating: 9.9,
         product: {
           upc: '1',
           name: 'Table',
@@ -14,33 +15,35 @@ export const seedData = {
             {
               id: '100',
               metric: 1,
-              data: 2
-            }
+              data: 2,
+            },
           ],
           objectCompoundKey: {
             id: '100',
             metric: 1,
-            data: 2
+            data: 2,
           },
           listCompoundKey: [
             {
               id: '100',
               metric: 1,
-              data: 2
-            }
-          ]
+              data: 2,
+            },
+          ],
+          value: 1,
         },
         authorID: '1',
         author: {
           id: '1',
           name: 'Ada Lovelace',
           username: '@ada',
-          numberOfReviews: 2
-        }
+          numberOfReviews: 2,
+        },
       },
       {
         id: '2',
         body: 'Too expensive.',
+        rating: 5.5,
         product: {
           upc: '2',
           name: 'Couch',
@@ -49,19 +52,21 @@ export const seedData = {
           inStock: false,
           metrics: [],
           objectCompoundKey: null,
-          listCompoundKey: []
+          listCompoundKey: [],
+          value: 2,
         },
         authorID: '1',
         author: {
           id: '1',
           name: 'Ada Lovelace',
           username: '@ada',
-          numberOfReviews: 2
-        }
+          numberOfReviews: 2,
+        },
       },
       {
         id: '3',
         body: 'Could be better.',
+        rating: 3.8,
         product: {
           upc: '3',
           name: 'Chair',
@@ -70,19 +75,21 @@ export const seedData = {
           inStock: true,
           metrics: [],
           objectCompoundKey: null,
-          listCompoundKey: []
+          listCompoundKey: [],
+          value: 3,
         },
         authorID: '2',
         author: {
           id: '2',
           name: 'Alan Turing',
           username: '@complete',
-          numberOfReviews: 2
-        }
+          numberOfReviews: 2,
+        },
       },
       {
         id: '4',
         body: 'Prefer something else.',
+        rating: 5.0,
         product: {
           upc: '1',
           name: 'Table',
@@ -93,30 +100,31 @@ export const seedData = {
             {
               id: '100',
               metric: 1,
-              data: 2
-            }
+              data: 2,
+            },
           ],
           objectCompoundKey: {
             id: '100',
             metric: 1,
-            data: 2
+            data: 2,
           },
           listCompoundKey: [
             {
               id: '100',
               metric: 1,
-              data: 2
-            }
-          ]
+              data: 2,
+            },
+          ],
+          value: 4,
         },
         authorID: '2',
         author: {
           id: '2',
           name: 'Alan Turing',
           username: '@complete',
-          numberOfReviews: 2
-        }
-      }
-    ]
-  }
+          numberOfReviews: 2,
+        },
+      },
+    ],
+  },
 };
