2bc2b56d23a84de6ebf6ef9c3070af96da42fe90
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2020-07-01 19:34:32 -0700

    Revert "adds relationship property data"
    This reverts commit 578aa89f3b342ed4b7cdfa0dd3cc823f4a2d81fe.

--- example/apollo-federation/seed-data.js
+++ example/apollo-federation/seed-data.js
@@ -4,7 +4,6 @@ export const seedData = {
       {
         id: '1',
         body: 'Love it!',
-        rating: 9.9,
         product: {
           upc: '1',
           name: 'Table',
@@ -15,35 +14,33 @@ export const seedData = {
             {
               id: '100',
               metric: 1,
-              data: 2,
-            },
+              data: 2
+            }
           ],
           objectCompoundKey: {
             id: '100',
             metric: 1,
-            data: 2,
+            data: 2
           },
           listCompoundKey: [
             {
               id: '100',
               metric: 1,
-              data: 2,
-            },
-          ],
-          value: 1,
+              data: 2
+            }
+          ]
         },
         authorID: '1',
         author: {
           id: '1',
           name: 'Ada Lovelace',
           username: '@ada',
-          numberOfReviews: 2,
-        },
+          numberOfReviews: 2
+        }
       },
       {
         id: '2',
         body: 'Too expensive.',
-        rating: 5.5,
         product: {
           upc: '2',
           name: 'Couch',
@@ -52,21 +49,19 @@ export const seedData = {
           inStock: false,
           metrics: [],
           objectCompoundKey: null,
-          listCompoundKey: [],
-          value: 2,
+          listCompoundKey: []
         },
         authorID: '1',
         author: {
           id: '1',
           name: 'Ada Lovelace',
           username: '@ada',
-          numberOfReviews: 2,
-        },
+          numberOfReviews: 2
+        }
       },
       {
         id: '3',
         body: 'Could be better.',
-        rating: 3.8,
         product: {
           upc: '3',
           name: 'Chair',
@@ -75,21 +70,19 @@ export const seedData = {
           inStock: true,
           metrics: [],
           objectCompoundKey: null,
-          listCompoundKey: [],
-          value: 3,
+          listCompoundKey: []
         },
         authorID: '2',
         author: {
           id: '2',
           name: 'Alan Turing',
           username: '@complete',
-          numberOfReviews: 2,
-        },
+          numberOfReviews: 2
+        }
       },
       {
         id: '4',
         body: 'Prefer something else.',
-        rating: 5.0,
         product: {
           upc: '1',
           name: 'Table',
@@ -100,31 +93,30 @@ export const seedData = {
             {
               id: '100',
               metric: 1,
-              data: 2,
-            },
+              data: 2
+            }
           ],
           objectCompoundKey: {
             id: '100',
             metric: 1,
-            data: 2,
+            data: 2
           },
           listCompoundKey: [
             {
               id: '100',
               metric: 1,
-              data: 2,
-            },
-          ],
-          value: 4,
+              data: 2
+            }
+          ]
         },
         authorID: '2',
         author: {
           id: '2',
           name: 'Alan Turing',
           username: '@complete',
-          numberOfReviews: 2,
-        },
-      },
-    ],
-  },
+          numberOfReviews: 2
+        }
+      }
+    ]
+  }
 };
