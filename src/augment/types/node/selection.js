08fa1bbb609d5eca732a99d915d1e3ba19f24598
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2020-10-31 10:53:35 -0700

    experimental support for where / data mutation arguments

--- src/augment/types/node/selection.js
+++ src/augment/types/node/selection.js
@@ -4,7 +4,8 @@ import {
   isNeo4jTypeField,
   isNeo4jIDField,
   unwrapNamedType,
-  TypeWrappers
+  TypeWrappers,
+  getTypeFields
 } from '../../fields';
 import {
   isCypherField,
@@ -15,6 +16,18 @@ import {
   isIndexedField,
   validateFieldDirectives
 } from '../../directives';
+import {
+  buildName,
+  buildNamedType,
+  buildInputValue,
+  buildInputObjectType
+} from '../../ast';
+import { shouldAugmentType } from '../../augment';
+import { OperationType } from '../../types/types';
+import {
+  buildPropertyFilters,
+  buildLogicalFilterInputValues
+} from '../../input-values';
 
 /**
  * Gets a single field for use as a primary key
@@ -111,4 +124,215 @@ const inferPrimaryKey = (fields = []) => {
     pk = fields[0];
   }
   return pk;
+};
+
+/**
+ * Builds the AST definition of the node input object type used
+ * by relationship mutations for selecting the nodes of the
+ * relationship
+ */
+export const buildNodeSelectionInputType = ({
+  definition,
+  typeName,
+  propertyInputValues,
+  generatedTypeMap,
+  typeExtensionDefinitionMap,
+  config
+}) => {
+  const mutationTypeName = OperationType.MUTATION;
+  const mutationTypeNameLower = mutationTypeName.toLowerCase();
+  if (shouldAugmentType(config, mutationTypeNameLower, typeName)) {
+    const fields = getTypeFields({
+      typeName,
+      definition,
+      typeExtensionDefinitionMap
+    });
+    const primaryKey = getPrimaryKey({ fields });
+    const propertyInputName = `_${typeName}Input`;
+    if (primaryKey) {
+      const primaryKeyName = primaryKey.name.value;
+      const primaryKeyInputConfig = propertyInputValues.find(
+        field => field.name === primaryKeyName
+      );
+      if (primaryKeyInputConfig) {
+        generatedTypeMap[propertyInputName] = buildInputObjectType({
+          name: buildName({ name: propertyInputName }),
+          fields: [
+            buildInputValue({
+              name: buildName({ name: primaryKeyName }),
+              type: buildNamedType({
+                name: primaryKeyInputConfig.type.name,
+                wrappers: {
+                  [TypeWrappers.NON_NULL_NAMED_TYPE]: true
+                }
+              })
+            })
+          ]
+        });
+      }
+    }
+  }
+  return generatedTypeMap;
+};
+
+export const buildNodeSelectionInputTypes = ({
+  definition,
+  typeName,
+  propertyInputValues,
+  generatedTypeMap,
+  typeDefinitionMap,
+  typeExtensionDefinitionMap,
+  config
+}) => {
+  const mutationTypeName = OperationType.MUTATION;
+  const mutationTypeNameLower = mutationTypeName.toLowerCase();
+  if (shouldAugmentType(config, mutationTypeNameLower, typeName)) {
+    const fields = getTypeFields({
+      typeName,
+      definition,
+      typeExtensionDefinitionMap
+    });
+    // Used by Create, Update, Merge
+    generatedTypeMap = buildNodeDataInputObject({
+      definition,
+      typeName,
+      propertyInputValues,
+      generatedTypeMap,
+      typeDefinitionMap,
+      typeExtensionDefinitionMap,
+      config
+    });
+    // Used by Update, Delete
+    generatedTypeMap = buildNodeSelectionInputObject({
+      definition,
+      typeName,
+      propertyInputValues,
+      generatedTypeMap,
+      typeDefinitionMap,
+      typeExtensionDefinitionMap,
+      config,
+      fields
+    });
+    // Used by Merge
+    generatedTypeMap = buildNodeKeySelectionInputObject({
+      definition,
+      typeName,
+      propertyInputValues,
+      generatedTypeMap,
+      typeDefinitionMap,
+      typeExtensionDefinitionMap,
+      config,
+      fields
+    });
+  }
+  return generatedTypeMap;
+};
+
+const buildNodeSelectionInputObject = ({
+  typeName,
+  generatedTypeMap,
+  typeDefinitionMap,
+  fields
+}) => {
+  let keyFields = getKeyFields({ fields });
+  keyFields = keyFields.filter(field => {
+    const directives = field.directives;
+    return (
+      isPrimaryKeyField({ directives }) ||
+      isUniqueField({ directives }) ||
+      isIndexedField({ directives })
+    );
+  });
+  if (!keyFields.length) {
+    const primaryKey = getPrimaryKey({ fields });
+    if (primaryKey) keyFields.push(primaryKey);
+  }
+  const propertyInputName = `_${typeName}Where`;
+  if (keyFields.length) {
+    const selectionArguments = buildLogicalFilterInputValues({
+      typeName: propertyInputName
+    });
+    keyFields.forEach(field => {
+      const fieldName = field.name.value;
+      const fieldType = field.type;
+      const unwrappedType = unwrapNamedType({ type: fieldType });
+      const outputType = unwrappedType.name;
+      const outputDefinition = typeDefinitionMap[outputType];
+      const outputKind = outputDefinition ? outputDefinition.kind : '';
+      selectionArguments.push(
+        ...buildPropertyFilters({
+          field,
+          fieldName,
+          outputType,
+          outputKind
+        })
+      );
+    });
+    generatedTypeMap[propertyInputName] = buildInputObjectType({
+      name: buildName({ name: propertyInputName }),
+      fields: selectionArguments
+    });
+  }
+  return generatedTypeMap;
+};
+const buildNodeKeySelectionInputObject = ({
+  typeName,
+  generatedTypeMap,
+  fields
+}) => {
+  let keyFields = getKeyFields({ fields });
+  keyFields = keyFields.filter(field => {
+    const directives = field.directives;
+    return (
+      isPrimaryKeyField({ directives }) ||
+      isUniqueField({ directives }) ||
+      isIndexedField({ directives })
+    );
+  });
+  if (!keyFields.length) {
+    const primaryKey = getPrimaryKey({ fields });
+    if (primaryKey) keyFields.push(primaryKey);
+  }
+  if (keyFields.length) {
+    const propertyInputName = `_${typeName}Keys`;
+    const selectionArguments = keyFields.map(field => {
+      const fieldName = field.name.value;
+      const fieldType = field.type;
+      const unwrappedType = unwrapNamedType({ type: fieldType });
+      const outputType = unwrappedType.name;
+      return buildInputValue({
+        name: buildName({ name: fieldName }),
+        type: buildNamedType({
+          name: outputType
+        })
+      });
+    });
+    generatedTypeMap[propertyInputName] = buildInputObjectType({
+      name: buildName({ name: propertyInputName }),
+      fields: selectionArguments
+    });
+  }
+  return generatedTypeMap;
+};
+
+const buildNodeDataInputObject = ({
+  typeName,
+  propertyInputValues,
+  generatedTypeMap
+}) => {
+  const propertyInputName = `_${typeName}Data`;
+  const inputValues = propertyInputValues.map(field => {
+    const { name, type } = field;
+    return buildInputValue({
+      name: buildName({ name }),
+      type: buildNamedType({
+        name: type.name
+      })
+    });
+  });
+  generatedTypeMap[propertyInputName] = buildInputObjectType({
+    name: buildName({ name: propertyInputName }),
+    fields: inputValues
+  });
+  return generatedTypeMap;
 };
