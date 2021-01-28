afcbcc9364a054f356ea8f77b42442e93fc14bc9
Michael Graham <38390185+michaeldgraham@users.noreply.github.com>
2021-01-27 18:24:07 -0800

    adds abstract subscription handlers to augmentation config

--- example/apollo-server/movies.js
+++ example/apollo-server/movies.js
@@ -1,7 +1,7 @@
 import { augmentTypeDefs, augmentSchema } from '../../src/index';
 import { ApolloServer, gql, makeExecutableSchema } from 'apollo-server';
 import neo4j from 'neo4j-driver';
-import { typeDefs, resolvers } from './movies-schema';
+import { typeDefs, resolvers, pubsub } from './movies-schema';
 
 const schema = makeExecutableSchema({
   typeDefs: augmentTypeDefs(typeDefs),
@@ -12,7 +12,19 @@ const schema = makeExecutableSchema({
 });
 
 // Add auto-generated mutations
-const augmentedSchema = augmentSchema(schema);
+const augmentedSchema = augmentSchema(schema, {
+  subscription: {
+    publish: (event, key, data) => {
+      pubsub.publish(event, {
+        [key]: data
+      });
+    },
+    subscribe: events => {
+      return pubsub.asyncIterator(events);
+    },
+    exclude: ['User']
+  }
+});
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI || 'bolt://localhost:7687',
